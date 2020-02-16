618f6d2cb4b051d569bba014a7131a0862096195
backlo <hjun4512@gmail.com>
2020-02-16 22:09:01 +0900

    feat: Item 모델 구현 - 구매 가능한 양인지 판별하는 기능 구현 - 아이템의 양이 주어졌을 때 종목을 확인해 class면 중복으로 사지 못하게 막고 kit면 살수 있게 기능 구현 - 구매한 상품의 수량을 재고수에서 빼는 기능 구현

---
+++ src/main/java/net/class101/server1/domain/Item.java
@@ -0,0 +1,90 @@
+package net.class101.server1.domain;
+
+import net.class101.server1.domain.exception.CannotBuyDuplicateClassException;
+import net.class101.server1.domain.exception.CannotBuyExcessAmountException;
+import net.class101.server1.domain.exception.SoldOutException;
+
+import java.util.Map;
+import java.util.Objects;
+
+public abstract class Item {
+    private static final String CLASS = "클래스";
+
+    private final int id;
+    private final String kind;
+    private final String name;
+    private final int price;
+    private final int stockNumber;
+
+    Item(int id, String kind, String name, int price, int stockNumber) {
+        this.id = id;
+        this.kind = kind;
+        this.name = name;
+        this.price = price;
+        this.stockNumber = stockNumber;
+    }
+
+    Item(int id, String name, int price, int stockNumber) {
+        this.id = id;
+        this.kind = "";
+        this.name = name;
+        this.price = price;
+        this.stockNumber = stockNumber;
+    }
+
+    public Item checkAvailableForPurchase(int itemAmount) {
+        if (this.stockNumber <= 0) {
+            throw new SoldOutException();
+        }
+        if (this.stockNumber < itemAmount) {
+            throw new CannotBuyExcessAmountException();
+        }
+        return this;
+    }
+
+    public Map<Item, Integer> getItemAmount(Map<Item, Integer> orderList, int itemAmount) {
+        if (this.kind.equals(CLASS)) {
+            throw new CannotBuyDuplicateClassException();
+        }
+        int beforeItemAmount = orderList.get(this);
+        orderList.put(this, beforeItemAmount + itemAmount);
+        return orderList;
+    }
+
+    public Item subtractStockNumber(int itemAmount) {
+        if (this.kind.equals(CLASS)) {
+            return this;
+        }
+        int afterItemStockNumber = this.stockNumber - itemAmount;
+        return new OrderItem(this.id, this.kind, this.name, this.price, afterItemStockNumber);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStockNumber() {
+        return stockNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
