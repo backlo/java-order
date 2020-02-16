2435933433b7848d758af203d492eceb14402b0e
backlo <hjun4512@gmail.com>
2020-02-17 01:59:48 +0900

    test: DBConnection 경로가 잘못입력했을 경우 테스트 추가

--- src/test/java/net/class101/server1/support/db/ConnectionManagerTest.java
+++ src/test/java/net/class101/server1/support/db/ConnectionManagerTest.java
@@ -1,13 +1,16 @@
 package net.class101.server1.support.db;
 
+import net.class101.server1.support.exception.CanNotReadFileException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConnectionManagerTest {
+    private static final String WRONG_PATH = "src/main/resources/wrong-data.sql";
 
     @Test
     @DisplayName("데이터 베이스 연결 테스트")
@@ -20,5 +23,11 @@ class ConnectionManagerTest {
         } catch (Exception e) {
             System.out.println("커넥션 실패");
         }
+    }
+
+    @Test
+    @DisplayName("데이터 베이스 연결 실패 테스트")
+    void getDataSourceExceptionTest() {
+        assertThrows(CanNotReadFileException.class, () -> ConnectionManager.getDataSource(WRONG_PATH));
     }
 }
